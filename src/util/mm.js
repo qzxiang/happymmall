55a943063f59360a6b6ac9e968f4a9731b613f7e
qzxiang <449240545@qq.com>
2017-09-29 19:43:56 +0800

    no message

--- src/util/mm.js
+++ src/util/mm.js
@@ -2,13 +2,14 @@
 * @Author: Marte
 * @Date:   2017-09-29 11:17:51
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-09-29 13:57:11
+* @Last Modified time: 2017-09-29 18:21:42
 */
 
 'use strict';
 
 var _mm = {
     request : function(param){
+        var _this = this;
         $.ajax({
             type     : param.method || 'get',
             url      : param.url    || '',
@@ -17,17 +18,23 @@ var _mm = {
             success  : function(res){
                 // 请求成功
                 if(0 == res.status){
-                    typeof param.success === 'function' && param.success(res.data, res.msg)
+                    typeof param.success === 'function' && param.success(res.data, res.msg);
                 }
                 // 没有登录状态，需要强制登录
                 else if(10 === res.status){
-
+                    _this.doLogin();
+                //请求数据错误
+                }else if(1 === res.status){
+                    typeof param.error === 'function' && param.error(res.msg);
                 }
             },
             error    : function(err){
-
+                typeof param.error === 'function' && param.error(err.statusText);
             }
         });
+    },
+    doLogin : funciton(){
+        window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href);
     }
 };
 
